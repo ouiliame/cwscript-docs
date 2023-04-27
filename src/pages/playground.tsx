a256de06214c74d21f48cbe4570215850da3845f
William Chen <william@terran.one>
2023-04-27 09:28:48 -0700

    refactor: Simplify and update dependencies in playground.tsx and package.json
    - Simplify and refactor `playground.tsx` component
    - Update dependencies in `package.json`
    - Fix formatting, update page title and CTA button in `index.tsx` for SEO purposes

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -4,10 +4,6 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
-import CodeMirror from "@uiw/react-codemirror";
-import { javascript } from "@codemirror/lang-javascript";
-import { classname } from "@uiw/codemirror-extensions-classname";
-
 import styles from "./index.module.css";
 
 function HomepageHeader() {
@@ -30,17 +26,6 @@ function HomepageHeader() {
   );
 }
 
-const classnameExt = classname({
-  add: (lineNumber) => {
-    if (lineNumber === 1) {
-      return "first-line";
-    }
-    if (lineNumber === 5) {
-      return "line-color";
-    }
-  },
-});
-
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   const [code, setCode] = useState<string>("");
@@ -56,14 +41,6 @@ export default function Home(): JSX.Element {
     <Layout
       title="CWScript Playground"
       description="Try CWScript in your browser."
-    >
-      <main className={styles.playground}>
-        <CodeMirror
-          value="console.log('hello world!');"
-          height="200px"
-          extensions={[classnameExt, javascript({ jsx: true })]}
-        />
-      </main>
-    </Layout>
+    ></Layout>
   );
 }
