aad7a4ca03dfe35c200e86c7a15cbe4c00add923
William Chen <william@terran.one>
2023-05-06 20:48:56 -0700

    fix colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,7 +82,7 @@ contract SimpleCounter {
 
 function HomepageFeatures() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
+    <div className="overflow-hidden bg-gray py-24 sm:py-32">
       <div className="mx-auto max-w-7xl md:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
           <div className="px-6 md:px-0 lg:pr-4 lg:pt-4">
@@ -90,25 +90,29 @@ function HomepageFeatures() {
               <h2 className="text-base font-semibold leading-7 text-orange-600">
                 A new language
               </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">
                 Build with CosmWasm
               </p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
+              <p className="mt-6 text-lg leading-8 text-gray-100">
                 CWScript is a language for writing smart contracts with
                 CosmWasm, designed to make building dApps projects on Cosmos
                 blockchains fun and easy.
               </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-white lg:max-w-none">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
+                    <dt className="inline font-semibold text-gray-200">
                       <feature.icon
                         className="absolute left-1 top-1 h-5 w-5 text-orange-600"
                         aria-hidden="true"
                       />
                       {feature.name}
                     </dt>
-                    <dd className="inline">{feature.description}</dd>
+                    <dd className="inline">
+                      <p className="text-base leading-7">
+                        {feature.description}
+                      </p>
+                    </dd>
                   </div>
                 ))}
               </dl>
@@ -117,29 +121,14 @@ function HomepageFeatures() {
           <div className="sm:px-6 lg:px-0">
             <div className="relative isolate overflow-hidden bg-orange-500 px-6 pt-8 sm:mx-auto sm:max-w-2xl sm:rounded-3xl sm:pl-16 sm:pr-0 sm:pt-16 lg:mx-0 lg:max-w-none">
               <div
-                className="absolute -inset-y-px -left-3 -z-10 w-full origin-bottom-left skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white"
+                className="absolute -inset-y-px -left-3 -z-10 w-full origin-bottom-left skew-x-[-32deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white"
                 aria-hidden="true"
               />
               <div className="mx-auto max-w-2xl sm:mx-0 sm:max-w-none">
-                <div className="w-screen overflow-hidden rounded-tl-xl bg-gray-900 ring-1 ring-white/10">
-                  <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                    <div className="-mb-px flex text-sm font-medium leading-6 text-gray-400">
-                      <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
-                        SimpleCounter.cws
-                      </div>
-                      <div className="border-r border-gray-600/10 px-4 py-2">
-                        CustomNFT.cws
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-6 pb-14 pt-6">
-                    <CodeBlockString
-                      language={"cwscript"}
-                      showLineNumbers={true}
-                    >
-                      {CWS_EXAMPLE}
-                    </CodeBlockString>
-                  </div>
+                <div className="px-6 pb-14 pt-6">
+                  <CodeBlockString language={"cwscript"} showLineNumbers={true}>
+                    {CWS_EXAMPLE}
+                  </CodeBlockString>
                 </div>
               </div>
               <div
